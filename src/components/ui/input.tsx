188ff3f7e6371da4896d8bea8a08d556654a9bbe
ThomasMaingre <thomas.maingre@epitech.eu>
2025-04-25 15:30:25 +0200

    Fix input.tsx: suppression placeholderColor et important

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: React.FC<InputProps> = ({ ...props }) => {
+export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <input
       {...props}
       className="border rounded px-3 py-2 w-full bg-[#9a9a9b] text-white placeholder-white focus:outline-none"
-      style={{ backgroundColor: '#9a9a9b', color: 'white', placeholderColor: 'white', borderColor: '#9a9a9b !important' }}
+      style={{ backgroundColor: '#9a9a9b', color: 'white', borderColor: '#9a9a9b' }}
     />
   )
 }
+
