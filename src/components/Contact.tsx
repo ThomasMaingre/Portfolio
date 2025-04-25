743e8bde13f55572a6768e685e5403e7911950b7
ThomasMaingre <thomas.maingre@epitech.eu>
2025-04-25 15:36:10 +0200

    Fix import Textarea: default import sans accolades

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { Send } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import Input from "@/components/ui/input"
-import { Textarea } from "@/components/ui/textarea"
+import Textarea from "@/components/ui/textarea"
 import { Label } from "@/components/ui/label"
 import emailjs from '@emailjs/browser'
 
